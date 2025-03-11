71b55d8b70f480dca83ebf1884025e68741136c0
marsevilspirit <marsevilspirit@gmail.com>
2025-03-12 01:42:24 +0800

    first success!!!

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -1,6 +1,17 @@
-(ns jepsen.etcdemo)
+(ns jepsen.etcdemo
+  (:require [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
+
+(defn etcd-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh, :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         {:pure-generators true}
+         opts))
 
 (defn -main
-  "Handles command line arguments. Can either run a test, or a web server for browsing results."
+  "Handles command line arguments.
+  Can either run a test, or a web server for browsing results."
   [& args]
-  (prn "Hello, world!" args))
+  (cli/run! (cli/single-test-cmd {:test-fn etcd-test})
+            args))
