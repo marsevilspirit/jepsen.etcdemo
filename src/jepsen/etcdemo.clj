fb4a899fa034215e0191cbfd6553e92e044d5301
marsevilspirit <marsevilspirit@gmail.com>
2025-03-12 10:56:17 +0800

    add cmd serve

--- src/jepsen/etcdemo.clj
+++ src/jepsen/etcdemo.clj
@@ -5,7 +5,7 @@
 (defn etcd-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh, :concurrency, ...), constructs a test map."
   [opts]
-  (merge tests/noop-test
+  (merge tests/noop-test ; noop-test means none test to run
          {:pure-generators true}
          opts))
 
@@ -13,5 +13,6 @@
   "Handles command line arguments.
   Can either run a test, or a web server for browsing results."
   [& args]
-  (cli/run! (cli/single-test-cmd {:test-fn etcd-test})
+  (cli/run! (merge (cli/single-test-cmd {:test-fn etcd-test}) ; main test
+                   (cli/serve-cmd)) ; serve a web browser to explore the history of our test results
             args))
